219e56c1a48929787aabd5deaa6e7b49092ef895
Your Name <you@example.com>
2021-08-08 14:17:23 +0300

    préservation

--- platform/deploy-minikube.sh
+++ platform/deploy-minikube.sh
@@ -6,13 +6,15 @@ set -o nounset
 set -o xtrace
 # set -eox pipefail #safety for script
 
-
+# https://www.digitalocean.com/community/tutorials/webinar-series-kubernetes-package-management-with-helm-and-ci-cd-with-jenkins-x
 echo "=============================Install minikube started ============================================================="
 sudo apt-get update -y
 #make sure that your system’s package manager can access packages over HTTPS with apt-transport-https
 sudo apt-get install -y apt-transport-https
 # in order to ensure the kubectl download is valid, add the GPG key for the official Google repository
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+# curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
 
 #show your system the source for downloading kubectl.
 cat <<EOT | sudo tee /etc/apt/sources.list.d/kubernetes.list
